c510a349d19804a4e563aa0a252128da4be170ee
diedonrrecaj12@gmail.com <diedonrrecaj12@gmail.com>
2025-02-01 13:26:37 +0100

    sql fix

--- CREATE DATABASE car selling.sql
+++ CREATE DATABASE car selling.sql
@@ -1,11 +1,15 @@
-CREATE DATABASE car_selling;
+
+CREATE DATABASE IF NOT EXISTS car_selling;
 
 USE car_selling;
 
-CREATE TABLE cars (
+CREATE TABLE IF NOT EXISTS cars (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     horsepower INT NOT NULL,
     image_path VARCHAR(255) NOT NULL
 );
+DROP TABLE IF EXISTS cars;
+
+
